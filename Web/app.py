1edaf8e6810fb22058283cde7c03bfa17d1fa2f0
brianna-mitri <brianna.mitri@gmail.com>
2025-03-26 01:00:13 -0700

    Added origin weather inputs with cyclical conversion for wind direction

--- Web/app.py
+++ Web/app.py
@@ -64,6 +64,23 @@ def predict():
         seat_cnt = request.form.get('seat_cnt')
         builder_certificated = request.form.get('builder_certificated')  #true or false
 
+        # Origin: Weather Info
+        precip_accum_1_hr = request.form.get('precip_accum_1_hr')
+        precip_accum_6_hr = request.form.get('precip_accum_6_hr')
+        air_temp = request.form.get('air_temp')
+        dew_point_temp = request.form.get('dew_point_temp')
+        relative_humidity = request.form.get('relative_humidity')
+        wind_speed = request.form.get('wind_speed')
+        wind_direction = request.form.get('wind_direction')
+        wind_gust = request.form.get('wind_gust')
+        visibility = request.form.get('visibility')
+
+        ceiling = request.form.get('ceiling')
+        ceiling_missing = 0  #default
+
+        sea_level_pressure = request.form.get('sea_level_pressure')
+        sea_level_pressure_missing = 0  #default
+
 
         # dest_airport = request.form.get('dest_airport')
         # weather = request.form.get('weather')
@@ -71,6 +88,7 @@ def predict():
         # ----------------------------------------
         # convert cyclical variables
         # ----------------------------------------
+        # time related 
         month_sin = math.sin(2 * math.pi * (parsed_date.month / 12.0))
         month_cos = math.cos(2 * math.pi * (parsed_date.month / 12.0))
 
@@ -82,6 +100,11 @@ def predict():
 
         minutes_sin = math.sin(2 * math.pi * total_minutes / 1440.0)
         minutes_cos = math.cos(2 * math.pi * total_minutes / 1440.0)
+
+        # wind direction (360 degrees)
+        wind_rad = np.deg2rad(wind_direction)
+        wind_dir_sin = np.sin(wind_rad)
+        wind_dir_cos = np.cos(wind_rad)
 
         # ----------------------------------------
         # convert into a DF
@@ -106,6 +129,20 @@ def predict():
             "Type of Engine": engine_type,
             "Number of Seats": seat_cnt,
             "Builder Type Certificated": int(builder_certificated),
+            "Precipitation Accumulation One Hour": float(precip_accum_1_hr),
+            "Precipitation Accumulation Six Hours": float(precip_accum_6_hr),
+            "Air Temperature": float(air_temp),
+            "Dew Point Temperature": float(dew_point_temp),
+            "Relative Humidity": float(relative_humidity),
+            "Wind Speed": float(wind_speed),
+            "Wind Direction (sin)": wind_dir_sin,
+            "Wind Direction (cos)": wind_dir_cos,
+            "Wind Gust": float(wind_gust),
+            "Visibility": float(visibility),
+            "Ceiling": int(ceiling),
+            "Ceiling Missing": int(ceiling_missing),
+            "Sea Level Pressure": float(sea_level_pressure),
+            "Sea Level Pressure Missing": int(sea_level_pressure_missing)
         }
 
         # convert all variables into a df
